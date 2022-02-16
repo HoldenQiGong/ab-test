1d7661c5e9b34964b17b17a5fe3b441627061ff4
Blayze Stone <blayzestone@protonmail.com>
2022-02-16 13:15:44 -0800

    feat: fetching url

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -4,5 +4,9 @@ export async function onRequest(context) {
   const { request } = context;
   const url = new URL(request.url);
 
-  return fetch(request);
+  return fetch(url);
 }
+
+addEventListener("fetch", (e) => {
+  e.respondWith(onRequest({ request: e.request }));
+});
