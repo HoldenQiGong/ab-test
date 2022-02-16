ab1de2110e156aa47e13fafe9c9f6279a30f18cc
Blayze Stone <blayzestone@protonmail.com>
2022-02-16 15:40:30 -0800

    test

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -4,7 +4,19 @@ export async function onRequest(context) {
   const { request } = context;
   const url = new URL(request.url);
 
-  return fetch(url);
+  // If there is no cookie, this is a new client. Choose a group and set the cookie.
+  const group = Math.random() < 0.5 ? "test" : "control"; // 50/50 split
+  // if (group === "control") {
+  //   url.pathname = "/control" + url.pathname;
+  // } else {
+  //   url.pathname = "/test" + url.pathname;
+  // }
+  // Reconstruct response to avoid immutability
+  let res = await fetch(url);
+  res = new Response(res.body, res);
+  // Set cookie to enable persistent A/B sessions.
+  // res.headers.append("Set-Cookie", `${NAME}=${group}; path=/`);
+  return res;
 }
 
 addEventListener("fetch", (e) => {
