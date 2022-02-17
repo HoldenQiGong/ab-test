b96ed18a07052a3864a71fb9db7321e46161ecee
Blayze Stone <blayzestone@protonmail.com>
2022-02-17 09:39:08 -0800

    test setup worker

--- functions/abtest.js
+++ functions/abtest.js
@@ -1,5 +1,3 @@
-const NAME = "myExampleWorkersABTest";
-
 export async function onRequest(context) {
   const { request } = context;
   const url = new URL(request.url);
@@ -16,10 +14,38 @@ export async function onRequest(context) {
   res = new Response(res.body, res);
 
   return res;
-  // Set cookie to enable persistent A/B sessions.
-  // res.headers.append("Set-Cookie", `${NAME}=${group}; path=/`);
 }
 
-// addEventListener("fetch", (e) => {
-//   e.respondWith(onRequest({ request: e.request }));
-// });
+// Durable Object
+export class ABVariantSwitch {
+  constructor(state, env) {
+    this.state = state;
+  }
+
+  // Handle HTTP requests from clients.
+  async fetch(request) {
+    // Durable Object storage is automatically cached in-memory, so reading the
+    // same key every request is fast. (That said, you could also store the
+    // value in a class member if you prefer.)
+    let variant = (await this.state.storage.get("variant")) || 0;
+
+    switch (variant) {
+      case "control":
+        variant = "test";
+        break;
+      case "test":
+        variant = "control";
+        break;
+      default:
+        variant = "control";
+    }
+
+    // We don't have to worry about a concurrent request having modified the
+    // value in storage because "input gates" will automatically protect against
+    // unwanted concurrency. So, read-modify-write is safe. For more details,
+    // see: https://blog.cloudflare.com/durable-objects-easy-fast-correct-choose-three/
+    await this.state.storage.put("variant", variant);
+
+    return new Response(variant);
+  }
+}
