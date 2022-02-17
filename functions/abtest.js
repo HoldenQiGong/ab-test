57499f5d3970384f45409d0a5683bd76e1dee51e
Blayze Stone <blayzestone@protonmail.com>
2022-02-16 16:21:30 -0800

    feat: ab test implementation

--- functions/abtest.js
+++ functions/abtest.js
@@ -6,17 +6,18 @@ export async function onRequest(context) {
 
   // If there is no cookie, this is a new client. Choose a group and set the cookie.
   const group = Math.random() < 0.5 ? "test" : "control"; // 50/50 split
-  // if (group === "control") {
-  //   url.pathname = "/control" + url.pathname;
-  // } else {
-  //   url.pathname = "/test" + url.pathname;
-  // }
+  if (group === "control") {
+    url.pathname = "/control";
+  } else {
+    url.pathname = "/test";
+  }
   // Reconstruct response to avoid immutability
-  // let res = await fetch(url);
-  // res = new Response(res.body, res);
+  let res = await fetch(url);
+  res = new Response(res.body, res);
+
+  return res;
   // Set cookie to enable persistent A/B sessions.
   // res.headers.append("Set-Cookie", `${NAME}=${group}; path=/`);
-  return new Response(`url: ${url}`);
 }
 
 // addEventListener("fetch", (e) => {
